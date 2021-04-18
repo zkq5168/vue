e23ae82c9a663c8f66d5dad82e4aad9e830e126d
zkq5168 <zkq5168@sina.com>
2021-04-18 15:41:36 +0800

    完成用户管理

--- fund/src/views/admin/User.vue
+++ fund/src/views/admin/User.vue
@@ -1,146 +1,283 @@
 <template>
-    <div class="user">
-        <el-form :inline="true" :model="formInline" class="demo-form-inline">
-            <el-form-item label="审批人">
-                <el-input v-model="formInline.user" placeholder="审批人"></el-input>
-            </el-form-item>
-            <el-form-item label="活动区域">
-                <el-select v-model="formInline.region" placeholder="活动区域">
-                <el-option label="区域一" value="shanghai"></el-option>
-                <el-option label="区域二" value="beijing"></el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="success" plain @click="onSubmit">查询</el-button>
-            </el-form-item>
-        </el-form>
-
-        <el-row class="btns">
-            <el-button type="primary" plain @click="dialogFormVisible = true">新增</el-button>
+    <div class="container">
+        <el-row>
+            <el-button type="primary" plain @click="addUser">新增</el-button>
         </el-row>
-
-        <el-dialog title="添加用户" :visible.sync="dialogFormVisible">
-            <el-form>
-                <el-form-item label="账号名称">
-                    <el-input v-model="form.accout" autocomplete="off"></el-input>
-                </el-form-item>
-                <el-form-item label="用户名称">
-                    <el-input v-model="form.username" autocomplete="off"></el-input>
-                </el-form-item>
-            </el-form>
-            <div slot="footer" class="dialog-footer">
-                <el-button @click="dialogFormVisible = false">取 消</el-button>
-                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-            </div>
-        </el-dialog>
-
         <el-row>
             <el-table
-                :data="tableData"
-                header-cell-class-name="table_header"
+                v-loading="loading"
+                :data="userList"
                 style="width: 100%">
                 <el-table-column
-                    label="用户名"
-                    width="180">
-                    <template slot-scope="scope">
-                        <p>{{ scope.row.name }}</p>
-                    </template>
+                    type="index"
+                    width="50px">
                 </el-table-column>
                 <el-table-column
-                    label="手机号码"
-                    width="180">
-                    <template slot-scope="scope">
-                        <p>{{ scope.row.phone }}</p>
-                    </template>
+                    label="用户账户"
+                    prop="account">
                 </el-table-column>
                 <el-table-column
-                    label="地址"
-                    width="580">
-                    <template slot-scope="scope">
-                        <p>{{ scope.row.address }}</p>
-                    </template>
+                    label="用户密码"
+                    prop="password">
                 </el-table-column>
-                <el-table-column label="操作">
+                <el-table-column
+                    label="用户类型"
+                    prop="userType">
+                </el-table-column>
+                <el-table-column
+                align="right">
+                <template slot="header" slot-scope="scope">
+                    <el-input
+                    v-model="searchTxt"
+                    @input="search"
+                    size="mini"
+                    placeholder="输入关键字搜索"/>
+                </template>
                 <template slot-scope="scope">
                     <el-button
                     size="mini"
-                    @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                    @click="editUser(scope.$index, scope.row)">编辑</el-button>
                     <el-button
                     size="mini"
                     type="danger"
-                    @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                    @click="deleteUser(scope.$index, scope.row)">删除</el-button>
                 </template>
                 </el-table-column>
             </el-table>
         </el-row>
-
-        <el-pagination
-            :background=true
-            layout="prev, pager, next"
-            :total="total">
-        </el-pagination>
+        <el-row>
+            <el-pagination
+                class="tr10"
+                background
+                layout="prev, pager, next"
+                @current-change="searchByPage"
+                :page-size = 8
+                :total="totalPage">
+            </el-pagination>
+        </el-row>
+        <el-dialog :title="dialogTitle" :visible.sync="showUserDialog" :isEdit="isEdit">
+            <el-form v-model="userForm">
+                <el-form-item label-width="100px" label="用户账户">
+                    <el-input v-model="userForm.account"></el-input>
+                </el-form-item>
+                <el-form-item label-width="100px" label="用户密码">
+                    <el-input v-model="userForm.password"></el-input>
+                </el-form-item>
+                <el-form-item label-width="100px" label="用户类型">
+                    <el-input v-model="userForm.userType"></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="showUserDialog = false">取 消</el-button>
+                <el-button type="primary" @click="isEdit ? updateUser() : saveUser()">确 定</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
 <script>
 export default {
     data() {
-      return {
-        formInline: {
-          user: '',
-          region: ''
-        },
-        form: {
-            account: '',
-            username: ''
-        },
-        dialogFormVisible: false,
-        tableData: [{
-            date: '2016-05-02',
-            name: '王小虎',
-            phone: '13900000001',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            phone: '13900000001',
-            address: '上海市普陀区金沙江路 1517 弄'
-          }, {
-            date: '2016-05-01',
-            name: '王小虎',
-            phone: '13900000001',
-            address: '上海市普陀区金沙江路 1519 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            phone: '13900000001',
-            address: '上海市普陀区金沙江路 1516 弄'
-        }],
-        total: 0
-      }
+        return {
+            //控制弹窗显示
+            showUserDialog: false,
+            //列表搜索关键字
+            searchTxt: '',
+            //对话框标题
+            dialogTitle: '',
+            //上传图片地址
+            imageUrl: '',
+            //总页数
+            totalPage: 1,
+            //是否修改
+            isEdit: false,
+            //列表数据
+            userList: [
+            // {
+            //     activityPic: '',
+            //     activityName: '活动',
+            //     mainTitle: '活动主题',
+            //     subTitle: '活动子标题'
+            // }
+            ],
+            //活动表单数据
+            userForm: {
+                userId: '',             //用户ID
+                account: '',            //用户账户
+                password: '',           //用户密码
+                userType: ''            //用户类型
+            },
+            //表格加载是否显示加载动画
+            loading: true
+        }
+    },
+    mounted() {
+        this.listUser(1)
     },
     methods: {
-      onSubmit() {
-        console.log('submit!');
-      },
-      addUser() {
+        searchByPage(val){
+            this.listUser(val);
+        },
+        //查询活动
+        listUser(pageNum, pageSize, searchTxt) {
+            let that = this;
+            var searchObj = {"pageNum": 1, "pageSize": 8, "param": null};
+            if(pageNum){
+                // pageNum = 1;
+                searchObj.pageNum = pageNum;
+            }
+            if(pageSize){
+                // pageSize = 3;
+                searchObj.pageSize = pageSize;
+            }
+            if(searchTxt){
+                searchObj.param = searchTxt;
+            }
 
-      }
+            this.axios.post("/apis/user/list", searchObj)
+            .then(res=>{
+                let data = res.data;
+                that.totalPage = data.totalNum;
+                if(data.code == 0){
+                    this.userList.splice(0);
+
+                    for(let i=0; i<data.result.length; i++){
+                        that.userList.push(data.result[i]);
+                    }
+                }else{
+                    that.$message(data.msg);
+                }
+            }).catch(e=>{
+                that.$message(e);
+            }).finally(() => {
+                that.loading = false;
+            });
+        },
+        //添加活动
+        addUser() {
+            this.dialogTitle = '新增用户';
+            this.showUserDialog = true;
+            this.isEdit = false;
+            //清空数据
+            for(let attr in this.userForm){
+                this.userForm[attr] = '';
+            }
+        },
+        //保存活动
+        saveUser() {
+            let that = this;
+            this.userForm.userPic = this.imageUrl;
+            this.axios.post("/apis/user/save", this.userForm)
+            .then(res=>{
+                let data = res.data;
+                if(data.code == 0){
+                    that.$message({
+                        type: "success",
+                        message: res.data.msg
+                    });
+                    that.showUserDialog = false;
+                    that.listUser();
+                }else{
+                    that.$message(res.data.msg);
+                }
+            })
+        },
+        //编辑活动
+        editUser(index, rowData) {
+            this.dialogTitle = '修改用户';
+            this.showUserDialog = true;
+            this.isEdit = true;
+            //获取数据
+            for(let attr in rowData){
+                this.userForm[attr] = rowData[attr];
+            }
+        },
+        updateUser(){
+            let that = this;
+            this.axios.post("/apis/user/update", this.userForm)
+                .then(res => {
+                    if(res.data.code == 0){
+                        that.$message({
+                            type: "success",
+                            message: res.data.msg
+                        });
+                        that.showUserDialog = false;
+                        that.listUser();
+                    }else{
+                        that.$message({
+                            type: "warning",
+                            message: res.data.msg
+                        });
+                    }
+                })
+        },
+        //删除活动
+        deleteUser(index,rowData) {
+            let that = this;
+            this.$confirm('是否确认删除该用户?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                that.axios.get("/apis/user/delete?id=" + rowData.userId)
+                .then(res=>{
+                    if(res.data.code == 0){
+                        that.$message({
+                            type: 'success',
+                            message: res.data.msg
+                        });
+                        //删除列表元素
+                        that.userList.splice(index, 1);
+                    }else{
+                        that.$message({
+                            type: 'warning',
+                            message: res.data.msg
+                        });
+                    }
+                }).catch(e => {
+                    that.$message({
+                        type: 'warning',
+                        message: res.data.msg
+                    });
+                });
+            }).catch(() => {
+                this.$message({
+                    type: 'info',
+                    message: '已取消删除!'
+                });          
+            });
+        },
+        search(value) {
+            this.listUser(null, null, value);
+        },
+        uploadSuccess(response, file, fileList) {
+            this.imageUrl = response.result.filePath+"/"+response.result.fileId+"."+response.result.fileExt;
+        }
     }
 }
 </script>
 
 <style scoped>
-.btns {
-    margin-bottom: 10px;
-}
-
 .el-table {
-    min-height: 450px;
+    min-height: 475px;
 }
 
-.el-pagination {
-    margin-top: 10px;
+.avatar-uploader {
+    border: 1px dashed #333;
+    border-radius: 6px;
+    cursor: pointer;
+    width: 100px;
+    height: 100px;
+}
+
+.avatar-uploader i {
+    width: 100px;
+    height: 100px;
+    line-height: 100px;
+}
+
+.tr10 {
     text-align: right;
+    margin-top: 10px;
 }
 </style>
